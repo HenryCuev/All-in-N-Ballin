c74de43d88e86000372b9aac3b9945340cfe9b5b
Henry Cuevas <cuevash1@login-i16.local>
2025-03-06 02:19:45 -0800

    modified nfsp to better work with hold em

--- rl_test_envs/nfsp.py
+++ rl_test_envs/nfsp.py
@@ -16,25 +16,39 @@ from open_spiel.python import rl_environment # type: ignore
 from open_spiel.python.algorithms import exploitability # type: ignore
 from open_spiel.python.algorithms import nfsp # type: ignore
 
+import numpy as np # type: ignore
+
 universal_poker = pyspiel.universal_poker
+
+# use a modification of NSFP to work with universal poker edge case
+class NFSP(nfsp.NFSP):
+
+  def _act(self, info_state, legal_actions):
+
+    info_state = np.reshape(info_state, [1, -1])
+    action_values, action_probs = self._session.run(
+        [self._avg_policy, self._avg_policy_probs],
+        feed_dict={self._info_state_ph: info_state})
+
+    self._last_action_values = action_values[0]
+    # Remove illegal actions, normalize probs
+    probs = np.zeros(self._num_actions)
+    # print()
+    probs[legal_actions] = action_probs[0][legal_actions]
+
+    if sum(probs) != 0:
+      probs /= sum(probs)
+    else:
+      # handling for edge case where probs dont line up
+      prob_per_action = 1/len(legal_actions)
+      probs = [prob_per_action if idx in legal_actions else 0 for idx in range(self._num_actions)]
+
+    action = np.random.choice(len(probs), p=probs)
+    
+    return action, probs
 
 FLAGS = flags.FLAGS
 
-CUSTOM_NO_LIMIT_HEADS_UP_TEXAS_HOLDEM_GAMEDEF = """\
-GAMEDEF
-nolimit
-numPlayers = 2
-numRounds = 4
-raiseSize = 20 20 20 20
-blind = 10 20
-firstPlayer = 1
-numSuits = 4
-numRanks = 13
-numHoleCards = 2
-numBoardCards = 0 3 1 1
-stack = 2000
-END GAMEDEF
-"""
 
 flags.DEFINE_integer("num_train_episodes", int(3e6),
                      "Number of training episodes.")
@@ -43,9 +57,9 @@ flags.DEFINE_integer("eval_every", 10000,
 flags.DEFINE_list("hidden_layers_sizes", [
     128,
 ], "Number of hidden units in the avg-net and Q-net.")
-flags.DEFINE_integer("replay_buffer_capacity", int(2e5),
+flags.DEFINE_integer("replay_buffer_capacity", int(2e7),
                      "Size of the replay buffer.")
-flags.DEFINE_integer("reservoir_buffer_capacity", int(2e6),
+flags.DEFINE_integer("reservoir_buffer_capacity", int(2e8),
                      "Size of the reservoir buffer.")
 flags.DEFINE_float("anticipatory_param", 0.1,
                    "Prob of using the rl best response as episode policy.")
@@ -55,9 +69,7 @@ class NFSPPolicies(policy.Policy):
   """Joint policy to be evaluated."""
 
   def __init__(self, env, nfsp_policies, mode):
-    game = universal_poker.load_universal_poker_from_acpc_gamedef(
-        CUSTOM_NO_LIMIT_HEADS_UP_TEXAS_HOLDEM_GAMEDEF
-    )
+    game = env.game
     player_ids = [0, 1]
     super(NFSPPolicies, self).__init__(game, player_ids)
     self._policies = nfsp_policies
@@ -83,11 +95,26 @@ class NFSPPolicies(policy.Policy):
 
 
 def main(unused_argv):
-  game = "kuhn_poker"
+  game = "universal_poker"
   num_players = 2
+  
+  CUSTOM_NO_LIMIT_HEADS_UP_TEXAS_HOLDEM_CONFIG = {
+    "betting":"nolimit",
+    "bettingAbstraction": "fcpa",
+    "numPlayers" : 2,
+    "numRounds" : 4,
+    "raiseSize" : "20 20 20 20",
+    "blind" : "10 20",
+    "firstPlayer" : "1",
+    "numSuits" : 4,
+    "numRanks" : 13,
+    "numHoleCards" : 2,
+    "numBoardCards" : "0 3 1 1",
+    "stack" : "2000 2000",
+  }
 
-  env_configs = {"players": num_players}
-  env = rl_environment.Environment(game, **env_configs)
+  env = rl_environment.Environment(game, **CUSTOM_NO_LIMIT_HEADS_UP_TEXAS_HOLDEM_CONFIG)
+
   info_state_size = env.observation_spec()["info_state"][0]
   num_actions = env.action_spec()["num_actions"]
 
@@ -102,7 +129,7 @@ def main(unused_argv):
   with tf.Session() as sess:
     # pylint: disable=g-complex-comprehension
     agents = [
-        nfsp.NFSP(sess, idx, info_state_size, num_actions, hidden_layers_sizes,
+        NFSP(sess, idx, info_state_size, num_actions, hidden_layers_sizes,
                   FLAGS.reservoir_buffer_capacity, FLAGS.anticipatory_param,
                   **kwargs) for idx in range(num_players)
     ]
@@ -113,8 +140,9 @@ def main(unused_argv):
       if (ep + 1) % FLAGS.eval_every == 0:
         losses = [agent.loss for agent in agents]
         logging.info("Losses: %s", losses)
-        expl = exploitability.exploitability(env.game, expl_policies_avg)
-        logging.info("[%s] Exploitability AVG %s", ep + 1, expl)
+        # NOTE: exploitability measure class no longer works, action space too big
+        # expl = exploitability.exploitability(env.game, expl_policies_avg)
+        # logging.info("[%s] Exploitability AVG %s", ep + 1, expl)
         logging.info("_____________________________________________")
 
       time_step = env.reset()
@@ -128,6 +156,10 @@ def main(unused_argv):
       for agent in agents:
         agent.step(time_step)
 
+    # save agent policy to files
+    # NOTE: saves only policy not training buffer
+    for pos, agent in enumerate(agents):
+      agent.save(f"agent_{pos+1}")
 
 if __name__ == "__main__":
   app.run(main)
